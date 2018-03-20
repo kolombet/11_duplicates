b9b1e866f31cfad38d17ddd75fd46c4e26dc61b0
anonra <anonrab@ya.ru>
2018-03-20 06:11:13 +0000

    style fix

--- duplicates.py
+++ duplicates.py
@@ -5,16 +5,16 @@ import hashlib
 import argparse
 
 
-def calcHash(name, size):
-    namesize = str({"name": name, "size": size}).encode("utf-8")
+def calc_hash(filename, size):
+    namesize = str({"filename": filename, "size": size}).encode("utf-8")
     val = hashlib.md5(namesize).hexdigest()
     return val
 
 
-def printResults(duplicates):
+def print_results(duplicates):
     files = duplicates.values()
     results = list(filter(lambda file: len(file) > 1, files))
-    if len(results) > 0:
+    if results:
         for result in results:
             print("Found coincidence between files:")
             for subresult in result:
@@ -44,11 +44,11 @@ if __name__ == "__main__":
             path = os.path.join(dirpath, filename)
 
             size = os.stat(path).st_size
-            file_hash = calcHash(filename, size)
+            file_hash = calc_hash(filename, size)
 
             if file_hash in duplicates:
                 duplicates[file_hash].append(path)
             else:
                 duplicates[file_hash] = [path]
-    printResults(duplicates)
+    print_results(duplicates)
     pass
