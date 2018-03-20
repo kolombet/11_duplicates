6e6c031e4564bdac0230ba48eb8d5c7b6741ac23
anonra <anonrab@ya.ru>
2018-03-20 05:51:20 +0000

    custom directory

--- duplicates.py
+++ duplicates.py
@@ -2,13 +2,14 @@ from os import walk
 from os import stat
 import os
 import hashlib
+import argparse
 
 def calcHash(name, size):
-    namesize = str({"name":name, "size":size}).encode('utf-8')
+    namesize = str({"name":name, "size":size}).encode("utf-8")
     val = hashlib.md5(namesize).hexdigest()
     return val
-    
-    
+
+
 def printResults(duplicates):
     files = duplicates.values()
     results = list(filter(lambda file: len(file) > 1, files))
@@ -19,12 +20,25 @@ def printResults(duplicates):
                 print("%s" % subresult)
     else:
         print('No coincidence found.')
-    
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-p",
+        "--path",
+        dest="path",
+        help="custom path to search duplicates",
+        default="default-path"
+    )
+    return parser.parse_args()
+
 
 if __name__ == "__main__":
     files = []
     duplicates = {}
-    for (dirpath, dirnames, filenames) in walk("test"):
+    args = get_args()
+    for (dirpath, dirnames, filenames) in walk(args.path):
         for filename in filenames:
             path = os.path.join(dirpath, filename)
             
