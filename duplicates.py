81b9be60cb75e1390ed12a459c4dc4bb95400a77
anonra <anonrab@ya.ru>
2018-03-20 06:06:17 +0000

    pycodestyle

--- duplicates.py
+++ duplicates.py
@@ -4,8 +4,9 @@ import os
 import hashlib
 import argparse
 
+
 def calcHash(name, size):
-    namesize = str({"name":name, "size":size}).encode("utf-8")
+    namesize = str({"name": name, "size": size}).encode("utf-8")
     val = hashlib.md5(namesize).hexdigest()
     return val
 
@@ -41,10 +42,10 @@ if __name__ == "__main__":
     for (dirpath, dirnames, filenames) in walk(args.path):
         for filename in filenames:
             path = os.path.join(dirpath, filename)
-            
+
             size = os.stat(path).st_size
             file_hash = calcHash(filename, size)
-            
+
             if file_hash in duplicates:
                 duplicates[file_hash].append(path)
             else:
