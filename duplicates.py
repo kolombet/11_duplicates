e881082e6777e8c7864bb8fc8406677782a90f1f
anonra <anonrab@ya.ru>
2018-03-20 09:16:06 +0000

    require to specify path (no default)

--- duplicates.py
+++ duplicates.py
@@ -1,11 +1,12 @@
 import os
 import argparse
+import sys
 from collections import defaultdict
 
 
-def find_duplicates():
+def find_duplicates(path):
     duplicates = defaultdict(list)
-    for dirpath, dirnames, filenames in os.walk(args.path):
+    for dirpath, dirnames, filenames in os.walk(path):
         for filename in filenames:
             path = os.path.join(dirpath, filename)
             size = os.stat(path).st_size
@@ -30,12 +31,13 @@ def get_args():
         "--path",
         dest="path",
         help="custom path to search duplicates",
-        default="test"
     )
     return parser.parse_args()
 
 
 if __name__ == "__main__":
     args = get_args()
-    duplicates = find_duplicates()
+    if not args.path:
+        sys.exit("please specify directory to search duplicates")
+    duplicates = find_duplicates(args.path)
     print_duplicates(duplicates)
